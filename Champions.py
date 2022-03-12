b2f427721450ea80bb49eaa3b2daae5c4a8e17c4
AidanInceer <aidaninceer0@gmail.com>
2022-03-12 16:46:35 +0000

    base_comit

--- Champions.py
+++
@@ -1,2 +0,0 @@
-champions = ["aatrox", "akali", "gangplank", "fiora", "camille", "kennen", "xayah", "rakan", "zac", "volibear",
-             "teemo", "orianna", "kayle", "tryndamere", "garen", "malphite", "maokai", "alistar"]
